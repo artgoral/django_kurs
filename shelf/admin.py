54d41eafe804e95a05f006f603573aa0252fa5c8
Artur Goral <artur.goral.informatyka@gmail.com>
2020-04-28 20:15:02 +0200

    aplikacja z ksiazkami - pierwsze podejscie

---
+++ shelf/admin.py
@@ -0,0 +1,11 @@
+# coding: UTF-8
+# python 2 only
+#from __future__ import unicode_laterals, absolute_import
+##############################
+
+from django.contrib import admin
+
+from .models import Author, Publisher, Book
+# Register your models here.
+
+admin.site.register([Author, Publisher, Book])
